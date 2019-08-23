85adcaa1a3bede4ac646997247c9c534526d65f7
Kesigomon <kesigomon@gmail.com>
2019-08-24 05:16:59 +0900

    add comment lec5-a.py

--- lec5-a.py
+++ lec5-a.py
@@ -8,7 +8,9 @@ async def on_message(message):
     # メッセージの送信者が、自分自身（BOT自身）であった場合
     if message.author == client.user:
         return  # 以降の部分を実行しない
+    # メッセージの内容が「こんにちは」で始まる場合
     if message.content.startswith("こんにちは"):
+        # メッセージが送られたチャンネルに「こんにちは！」と送信する
         await message.channel.send("こんにちは！")
 
 # トークンを入れてね
